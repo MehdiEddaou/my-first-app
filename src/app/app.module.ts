364d765fa75057587b950f8edd81083df837a8e1
Mehdi ED-Daou <mehddaw2005@gmail.com>
2021-04-06 22:55:30 +0100

    add bootstrap and jquery to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './core/menu/menu.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HeaderComponent } from './core/header/header.component';
+const appRoutes: Routes = [
+  
+  ];
 
 @NgModule({
   declarations: [
-    AppComponent
+    
+    AppComponent,
+    MenuComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
