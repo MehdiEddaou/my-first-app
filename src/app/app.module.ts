4df2eb9360c20ff18e9b8588a4cdaceb2719b2a3
Mehdi ED-Daou <mehddaw2005@gmail.com>
2021-04-08 00:44:27 +0100

    set login and registration and dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutModule } from './layout/layout.module';
+
 const appRoutes: Routes = [
   
   ];
