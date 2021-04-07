705bfd586772cd0b486637bd4810a76f53e138e4
Mehdi ED-Daou <mehddaw2005@gmail.com>
2021-04-08 00:33:58 +0100

    set up the layout of app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-import { MenuComponent } from './core/menu/menu.component';
-import { FooterComponent } from './core/footer/footer.component';
 import { RouterModule, Routes } from '@angular/router';
-import { HeaderComponent } from './core/header/header.component';
+import { LayoutModule } from './layout/layout.module';
 const appRoutes: Routes = [
   
   ];
 
 @NgModule({
   declarations: [
-    
     AppComponent,
-    MenuComponent,
-    FooterComponent,
-    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(appRoutes),
+    RouterModule.forRoot([]),
+    // RouterModule.forRoot([], { preloadingStrategy: PreloadAllModules }),
+    LayoutModule,
   ],
   exports: [RouterModule],
   providers: [],
